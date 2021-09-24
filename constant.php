0fb88a76760bb4409f30f12120c1828b347defe9
Alita193040125 <63622938+Alita193040125@users.noreply.github.com>
2021-09-24 15:42:42 +0700

    Create constant.php
    Menyelesaikan video tentang constant

---
+++ constant.php
@@ -0,0 +1,30 @@
+<?php
+*/
+Alita Eugenia
+193040125
+Pertemuan 3
+/*
+
+// define ('NAMA', 'Alita Eugenia');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 20;
+// echo UMUR;
+
+// 
+// echo __LINE__; //menampilkan baris
+// echo __FILE__; //menampilkan alamat file
+
+// function coba() {
+//     return __FUNCTION__;
+//     echo coba();
+// }
+
+class coba {
+    public $kelas = __CLASS__;
+}
+    $obj = new coba;
+    echo $obj->kelas;
+?>
